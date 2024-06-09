03ea65db3d4ddc2dced2a0d7d1c5820d22c64857
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-06-09 21:47:57 +0200

    Added route path and validation in Entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,5 @@
 class Entry < ApplicationRecord
+    validates :title, presence: { message: "Please enter title."}
+    validates :date, presence: { message: "Please enter date."}
+    validates :body, presence: { message: "Please enter body."}
 end
